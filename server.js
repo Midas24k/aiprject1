b4753456acf8b0119796b518a6a3ec3cd84f323e
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-05-12 23:32:39 -0400

    started the script for server.js

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
+const { OpenAI } = require("@langchain/openai");
 require('dotenv').config();
 
-console.log(process.env.OPENAI_API_KEY);
+const model = new OpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    temperature: 0,
+    model: 'gpt-3.5-turbo',
+});
+
+const promptFunc = async (input) => {
+    try {
+        const res = await model.invoke(input);
+        return res;
+    }
+    catch (err) {
+        console.log(err);
+        throw err;
+    }
+};
+
+//Test
+console.log(promptFunc("How do you capitalize all characters of a string in JavaScript?"));
